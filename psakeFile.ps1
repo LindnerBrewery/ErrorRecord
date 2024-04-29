ea41d5c24a033cf54c0cacee692305049294ee1e
Emrys MacInally <emrys@macinally.de>
2024-04-29 11:07:17 +0200

    Update CI.yaml to install GitVersion and fetch the complete history Add testResults.xml to .gitignore Update psakeFile.ps1 to include a build task that updates the psd1 version

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -5,13 +5,22 @@ properties {
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
 }
 
-task Default -depends Test
+task Default -depends updateversion, build, Test
 
-task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+task Test -FromModule PowerShellBuild -minimumVersion '0.6.1' -depends UpdateVersion, build
 task build -FromModule PowerShellBuild -minimumVersion '0.6.1' -depends UpdateVersion
 Task UpdateVersion  {
     $file = "$env:BHModulePath\$env:BHProjectName.psd1"
     $newVersion = dotnet-gitversion /showvariable MajorMinorPatch
     $prereleaseVersion = (dotnet-gitversion /showvariable NuGetPreReleaseTag) -replace "-",""
-    update-ModuleManifest -Path $file  -ModuleVersion $newVersion
+    if (! $prereleaseVersion) {
+        $prereleaseVersion = ' '
+    }
+    update-ModuleManifest -Path $file  -ModuleVersion $newVersion -Prerelease $prereleaseVersion
+    $version = Get-ManifestValue -Path $file  -PropertyName moduleversion
+    $prereleaseVersion = Get-ManifestValue -Path $file -PropertyName prerelease -ErrorAction SilentlyContinue
+    if ($prereleaseVersion) {
+        $version = ("{0}-{1}" -f $version, $prereleaseVersion)
+    }
+    Write-Host ("Version is: {0}" -f $version)
 } -description 'updates the psd1 version in releases'
